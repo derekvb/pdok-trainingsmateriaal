448b3fc944d7af69ececc401c31ee32a3d3db5a0
Derek van Bochove <50830605+derekvb@users.noreply.github.com>
2025-07-22 14:37:00 +0200

    Create main.js

---
+++ voorbeeld/maplibre/main.js
@@ -0,0 +1,17 @@
+import * as maplibregl from "https://esm.sh/maplibre-gl";
+import { Protocol } from "https://esm.sh/pmtiles";
+const protocol = new Protocol();
+maplibregl.addProtocol("pmtiles", protocol.tile);
+
+const map = new maplibregl.Map({
+    container: 'map', // container id
+    style: './assets/style.json', // style URL
+    center: [5.66509, 51.96857], // starting position [lng, lat]
+    zoom: 13, // starting zoom
+    maxBounds: [
+        [],
+        []
+    ],
+    minZoom: 8,
+    maxZoom: 16,
+});
